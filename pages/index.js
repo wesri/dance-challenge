91edab5e58aad0969ed11e6780fa3953e271b9ea
Riku Wessman <riku.wessman@aalto.fi>
2023-11-11 20:45:20 +0200

    edit

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>Challenge: Wednesday Dance</h1>
         <h1 className={styles.description}>Hold your phone on your right hand.</h1>
-        <video src="IMG_4720.mov" autoplay muted width="200" height="420" controls>
+        <video src="https://drive.google.com/uc?export=download&id=1ZspDJy6LqsitQr6lzv5YNYmmLBlS9PyW" autoplay muted width="200" height="420" controls>
           Your browser does not support the video tag.
         </video>
         <Accelerometer />
