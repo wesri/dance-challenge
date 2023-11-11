7d960f20d730d3fdd6b617d1ba6c562a02acf64d
Riku Wessman <riku.wessman@aalto.fi>
2023-11-11 14:46:57 +0200

    Use gyro and geomagnetic as well

--- components/Accelerometer.js
+++ components/Accelerometer.js
@@ -2,11 +2,25 @@ import { useState } from 'react';
 
 function Accelerometer() {
     const [isRunning, setIsRunning] = useState(false);
-    const [data, setData] = useState([]);
+    const [data, setData] = useState({ accel: {}, gyro: {}, mag: {} });
 
     const handleMotionEvent = (event) => {
         const { x, y, z } = event.acceleration;
-        setData(currentData => [...currentData, { x, y, z }]);
+        setData(currentData => ({ ...currentData, accel: { x, y, z } }));
+    };
+
+    const handleOrientationEvent = (event) => {
+        const { alpha, beta, gamma } = event;
+        setData(currentData => ({ ...currentData, gyro: { alpha, beta, gamma } }));
+    };
+
+    const handleCompassEvent = (event) => {
+        // Note: This event may not provide detailed data like the others
+        const { webkitCompassHeading, webkitCompassAccuracy } = event;
+        setData(currentData => ({
+            ...currentData,
+            mag: { heading: webkitCompassHeading, accuracy: webkitCompassAccuracy }
+        }));
     };
 
     const startDemo = () => {
@@ -15,17 +29,23 @@ function Accelerometer() {
                 .then(permissionState => {
                     if (permissionState === 'granted') {
                         window.addEventListener('devicemotion', handleMotionEvent);
+                        window.addEventListener('deviceorientation', handleOrientationEvent);
+                        window.addEventListener('compassneedscalibration', handleCompassEvent);
                     }
                 })
                 .catch(console.error);
         } else {
             window.addEventListener('devicemotion', handleMotionEvent);
+            window.addEventListener('deviceorientation', handleOrientationEvent);
+            window.addEventListener('compassneedscalibration', handleCompassEvent);
         }
         setIsRunning(true);
     };
 
     const stopDemo = () => {
         window.removeEventListener('devicemotion', handleMotionEvent);
+        window.removeEventListener('deviceorientation', handleOrientationEvent);
+        window.removeEventListener('compassneedscalibration', handleCompassEvent);
         setIsRunning(false);
 
         // Here you can handle the collected data, like sending it to the server
@@ -34,19 +54,13 @@ function Accelerometer() {
 
     return (
         <div>
+            <button onClick={isRunning ? stopDemo : startDemo}>
+                {isRunning ? 'Stop Demo' : 'Start Demo'}
+            </button>
             <div>
-                <button onClick={isRunning ? stopDemo : startDemo}>
-                    {isRunning ? 'Stop Demo' : 'Start Demo'}
-                </button>
-            </div>
-            <div>
-                {data.length > 0 && (
-                    <p>
-                        X: {data[data.length - 1].x.toFixed(2)}, 
-                        Y: {data[data.length - 1].y.toFixed(2)}, 
-                        Z: {data[data.length - 1].z.toFixed(2)}
-                    </p>
-                )}
+                <p>Accelerometer - X: {data.accel.x?.toFixed(2)}, Y: {data.accel.y?.toFixed(2)}, Z: {data.accel.z?.toFixed(2)}</p>
+                <p>Gyroscope - Alpha: {data.gyro.alpha?.toFixed(2)}, Beta: {data.gyro.beta?.toFixed(2)}, Gamma: {data.gyro.gamma?.toFixed(2)}</p>
+                <p>Magnetometer - Heading: {data.mag.heading?.toFixed(2)}, Accuracy: {data.mag.accuracy?.toFixed(2)}</p>
             </div>
         </div>
     );
